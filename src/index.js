eb7e6f0531a16f8d5059a06611891dae2e66c2df
Rajesh kumar <rakumarv87@gmail.com>
2023-11-04 19:04:42 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from 'react-router-dom';
-import MainComponent from './components/SearchSathi/mainComponent';
+import MainComponent from './components/mainComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
